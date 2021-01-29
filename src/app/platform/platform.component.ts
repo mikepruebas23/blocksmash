661c3bdde1e095edf29bfb034b03288699a9dac6
mikepruebas23 <mikepruebas23@gmail.com>
2021-01-29 13:55:49 -0700

    se agrega las clases a las box

--- src/app/platform/platform.component.ts
+++ src/app/platform/platform.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Inject }  from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 
 @Component({
   selector: 'app-platform',
@@ -7,7 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatformComponent implements OnInit {
 
-  constructor() { }
+  boxId: string;
+  @ViewChild('') myDiv: ElementRef;
+
+  constructor(@Inject(DOCUMENT) document) { }
 
   boxElements: {};
 
@@ -15,29 +22,97 @@ export class PlatformComponent implements OnInit {
     this.drawGrid();
   }
 
+  //show all elements
   drawGrid(){
+    // console.log(this.randomNumber());
     this.boxElements = [
       {
         number: 1,
         class: "green",
-        value: true
+        value: true,
+        rndom: this.randomNumber()
       },
       {
         number: 2,
         class: "red",
-        value: false
+        value: false,
+        rndom: this.randomNumber()
       },
       {
         number: 3,
         class: "green",
-        value: true
+        value: true,
+        rndom: this.randomNumber()
       },
       {
         number: 4,
         class: "red",
-        value: false
+        value: false,
+        rndom: this.randomNumber()
+      },
+      {
+        number: 5,
+        class: "green",
+        value: true,
+        rndom: this.randomNumber()
+      },
+      {
+        number: 6,
+        class: "red",
+        value: false,
+        rndom: this.randomNumber()
+      },
+      {
+        number: 7,
+        class: "green",
+        value: true,
+        rndom: this.randomNumber()
+      },
+      {
+        number: 8,
+        class: "red",
+        value: false,
+        rndom: this.randomNumber()
       }
     ];
+
+
+    let i;
+    let boxLength = Object.keys( this.boxElements).length; 
+    // console.log();
+    for(i = 0; i < boxLength; i++){
+      if(this.boxElements[i].rndom == 2){
+        this.boxElements[i].class = "red";
+      }
+      else {
+        this.boxElements[i].class = "green";
+      }
+      console.log(this.boxElements[i]);
+    }
+  }
+
+  //select actual box
+  selectBox(index){
+    let box = this.boxElements[index];
+    // console.log(index);
+    // console.log(this.boxElements[index]);
+    this.flipBox(box)
+  }
+
+  //Flip selected box
+  flipBox(box){
+    
+    let boxElement = document.getElementById(box.number);
+    // let boxId = this.myDiv.id = '5'
+    let bloxClass = box.class;
+    boxElement.classList.add(bloxClass);
+    // console.log(boxElement);
+  }
+
+  //generate number random
+  randomNumber(){
+    let rndm = Math.floor(Math.random() * 2) + 1;
+    return rndm;
   }
 
 }
